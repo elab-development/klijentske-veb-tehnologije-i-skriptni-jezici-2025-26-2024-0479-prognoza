81fc4ab2ac2f2adeb4c1535357962673148acc0f
Angelina Kupreščanin <angelinakuprescanin@gmail.com>
2026-08-28 15:00:44 +0200

    Ispravka layout-a

--- vremenska-prognoza/src/components/Layout.tsx
+++ vremenska-prognoza/src/components/Layout.tsx
@@ -5,11 +5,15 @@ import Footer from './Footer';
 
 export default function Layout() {
   return (
-    <div className='min-h-screen bg-[#272745] text-slate-900'>
+    <div className='min-h-screen flex flex-col bg-slate-50 text-slate-900'>
       <Navbar />
-      <main className='container mx-auto px-4 py-6'>
-        <Outlet />
+
+      <main className='flex-1 flex items-center justify-center'>
+        <div className='container mx-auto px-4 py-6'>
+          <Outlet />
+        </div>
       </main>
+
       <Footer />
     </div>
   );
